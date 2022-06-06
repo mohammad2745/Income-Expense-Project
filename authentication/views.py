3b080b6effa3a2b64fb5144803fbc7eaebe57f34
pran2745 <mohammadfcc2745@gmail.com>
2022-06-07 02:03:13 +0600

    Username and Email Validation using Ajax

--- authentication/views.py
+++ authentication/views.py
@@ -1,8 +1,10 @@
+import email
 from django.shortcuts import render
 from django.views import View
 import json
 from django.http import JsonResponse
 from django.contrib.auth.models import User
+from validate_email import validate_email
 
 
 class RegistrationView(View):
@@ -22,3 +24,17 @@ class UsernameValidationView(View):
             return JsonResponse({'username_error':'Username exists already.'}, status=409)
     
         return JsonResponse({'username_valid':True})
+
+
+class EmailValidationView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        email = data['email']
+
+        if not validate_email(email):
+            return JsonResponse({'email_error':'Email is invalid'}, status=400)
+
+        if User.objects.filter(email=email).exists():
+            return JsonResponse({'email_error':'Email exists already.'}, status=409)
+    
+        return JsonResponse({'email_valid':True})
--- incomeExpense/static/js/register.js
+++ incomeExpense/static/js/register.js
@@ -1,7 +1,37 @@
 const usernameField = document.getElementById('usernameField');
 const feedbackFieldArea = document.querySelector('.invalid-feedback')
+const emailField = document.querySelector('#emailField')
+const emailFeedbackArea = document.querySelector('.emailFeedbackArea')
+const usernameSuccessOutput = document.querySelector('.usernameSuccessOutput')
+
+// Email Validation
+emailField.addEventListener('keyup', (e) => {
+    const emailVal = e.target.value;
+    usernameSuccessOutput.style.display = 'none'
+
+    emailField.classList.remove("is-invalid");
+    emailFeedbackArea.style.display = 'none';
+
+    if (emailVal.length > 0) {
+        fetch("/authentication/validate-email", {
+            body: JSON.stringify({ email: emailVal }),
+            method: "POST"
+        }).then(res => res.json()).then(data => {
+            console.log('data', data)
+            if (data.email_error) {
+                emailField.classList.add("is-invalid");
+                emailFeedbackArea.style.display = 'block';
+                emailFeedbackArea.innerHTML = `<p>${data.email_error}</p>`
+            }
+        });
+    }
+});
+
+
+// Username Validation
 usernameField.addEventListener('keyup', (e) => {
     const usernameVal = e.target.value;
+    usernameSuccessOutput.textContent = `Checking ${usernameVal}`;
 
     usernameField.classList.remove("is-invalid");
     feedbackFieldArea.style.display = 'none';
@@ -11,6 +41,7 @@ usernameField.addEventListener('keyup', (e) => {
             body: JSON.stringify({ username: usernameVal }),
             method: "POST"
         }).then(res => res.json()).then(data => {
+            usernameSuccessOutput.style.display = 'none'
             console.log('data', data)
             if (data.username_error) {
                 usernameField.classList.add("is-invalid");
